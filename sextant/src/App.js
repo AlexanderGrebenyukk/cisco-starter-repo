bc8780faa3228df0959f35c8e5d1a8ad7f0d92f1
Alexander Grebenyuk <alexander.grebenyuk22@gmail.com>
2023-09-18 12:26:01 +0100

    Make banner and exhibit component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+const Banner = ({ title }) => {
+  return (
+    <div className="banner">
+      <h1>{title}</h1>
+    </div>
+  );
+};
+const Exhibit = ({ title, children }) => {
+  return (
+    <div className="exhibit">
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Banner title={"Sextant"}/>
   );
 }
 
